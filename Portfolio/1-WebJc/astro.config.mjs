9821f306d1fdc530822059ac132965f618386352
Carlos Julio Castaño Torres <118862658+CarlosjctDev@users.noreply.github.com>
2025-08-05 18:18:29 -0500

    Update astro.config.mjs

--- Portfolio/1-WebJc/astro.config.mjs
+++ Portfolio/1-WebJc/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import github from '@astrojs/github';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: github(),
   base: '/Portfolio/1-WebJc/',  
   vite: {
     plugins: [tailwindcss()]
